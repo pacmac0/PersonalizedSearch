6f6940e5a4aaf076dc01eda14e66762a26983c5a
Fynn <fynn.van-westen@gmx.de>
2021-03-31 13:05:29 +0200

    added import script for elastic data

---
+++ import_data_to_elastic.sh
@@ -0,0 +1,5 @@
+#!bin/sh
+for FILE in "train_import_10000.json" "train_import_20000.json" "train_import_30000.json" "train_import_40000.json" "train_import_50000.json" "train_import_60000.json" "train_import_70000.json" "train_import_80000.json" "train_import_90000.json" "train_import_100000.json" "train_import_110000.json"
+do
+	curl -H "Content-Type: application/json" -XPOST "localhost:9200/news/_bulk?pretty&refresh" --data-binary "@${FILE}"
+done
