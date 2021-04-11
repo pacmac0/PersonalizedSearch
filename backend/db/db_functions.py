8515a635908bff0fd93b919f01923c15d6110d48
Artin Mirzaian <mirzaian@kth.se>
2021-04-11 17:38:39 +0200

    solid base for backend

--- backend/db/db_functions.py
+++ backend/db/db_functions.py
@@ -1,8 +1,10 @@
+from .utils import normalize_vec
 from flask import request, g
 from flask import make_response
 import json
 JSON_MIME_TYPE = 'application/json; charset=utf-8'
-
+from elasticsearch import Elasticsearch
+es = Elasticsearch()
 
 def success_response(result, message=''):
     format = {'status': 'success',
@@ -22,9 +24,24 @@ def success_message(message):
 
     return json_response(json.dumps(format))
 
+def update_user():
+    data = request.args
+    body = {
+        "script": {
+            "source": "if (ctx._source.history.contains(params.click)) { ctx._source.history.remove(ctx._source.history.indexOf(params.click))} ctx._source.history.add(params.click)",
+            "lang": "painless",
+            "params": {
+            "click": data["click"]
+            }
+         }
+    }   
+    results = es.update(index="users", id=data["id"], body=body)
+    return success_response(results)
 
 # This method is to fetch the students details
 def get_user():
+    data = request.args
+    """
     if request.method == 'POST':
         data = request.args
         user_cursor = g.db.execute("SELECT * FROM "
@@ -35,6 +52,81 @@ def get_user():
         user_data = [{'id': row[0], 'vector': row[1]} for row in data]
 
         return success_response(user_data)
+    """
+    body = {
+            "query": {
+                "match": {"user_id":data["id"]}
+            }
+        }
+    results = es.search(index = "users", body=body)
+    return success_response(results)
+
+def get_recommendations():
+    data = request.args
+    body = {
+    "query": {
+        "bool": {
+        "must": {
+            "term": {
+            "history.keyword": data["id"]
+            }
+        }
+        }
+    },
+    "aggs": {
+        "recommendations": {
+        "significant_terms": {
+            "field": "history.keyword",
+            "exclude": data["id"],
+            "min_doc_count": 100
+        }
+        }
+        }
+    }
+    results = es.search(index = "users", body = body)
+    return success_response(results["aggregations"]["recommendations"]["buckets"])
+
+def get_user_vector():
+    data = request.args
+    news_fields = ['title','category','body']
+
+    body = {
+        "ids": json.loads(data["news"]),
+        "parameters": {
+            "fields": news_fields,
+            "offsets" : False,
+            "payloads" : False,
+            "positions" : False,
+            "term_statistics" : True,
+            "field_statistics": True,
+            "filter": {
+                "min_term_freq": 1,
+            }
+        }
+    }
+    results = es.mtermvectors(body=body, index="news")
+    ret = dict()
+    for c in news_fields:
+        ret[c] = dict()
+    for doc in results['docs']:
+        if "term_vectors" in doc:
+            for k in news_fields:
+                # some news haven't a field (N47482 haven't a text)
+                if k in doc["term_vectors"]:
+                    term_vec = doc["term_vectors"][k]["terms"]
+                    for t, t_value in term_vec.items():
+                        if t in ret[k]:
+                            ret[k][t] += t_value["score"]
+                        else:
+                            ret[k][t] = t_value["score"]
+    
+    # Normalize
+    for key, value in ret.items():
+        ret[key] = normalize_vec(value)
+
+    # Aggregate ? 
+
+    return success_response(ret)
 
 def store_user():
     if request.method == 'POST':
--- backend/route_config.py
+++ backend/route_config.py
@@ -1,4 +1,4 @@
-from db.db_functions import get_user, store_user
+from db.db_functions import get_user, get_recommendations, get_user_vector, update_user
 from db.db_config import get_db
 from flask import Flask
 from db.db_functions import *
@@ -12,13 +12,22 @@ def before_request():
 # This method returns students 
 # list and by default method will be GET
 @app.route('/api/getuser', methods = ['POST'])
-def get_students_list():
+def getuser():
     return get_user()
-# This is POST method which stores students details.
-@app.route('/api/storeuser', methods=['POST'])
-def store_student_data():
-    return store_user()
+
+@app.route('/api/updateuser', methods=['POST'])
+def updateuser():
+    return update_user()
+
+@app.route('/api/getrecommendation', methods=['POST'])
+def getrecommendations():
+    return get_recommendations()
+
+@app.route('/api/getuservector', methods=['POST'])
+def getuservector():
+    return get_user_vector()
 # This method executes after every API request.
 @app.after_request
 def after_request(response):
     return response
+
