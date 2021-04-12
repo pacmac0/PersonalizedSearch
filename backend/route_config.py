d55cfa4612ec628d24286501204c70b93c427105
Artin Mirzaian <mirzaian@kth.se>
2021-04-12 14:56:39 +0200

    removed some unnecessary files

--- backend/route_config.py
+++ backend/route_config.py
@@ -1,14 +1,9 @@
-from db.db_functions import get_user, get_recommendations, get_user_vector, update_user
-from db.db_config import get_db
 from flask import Flask
-from db.db_functions import *
+from api.api import *
 # app reference
 app = Flask(__name__)
 
-# This method executes before any API request
-@app.before_request
-def before_request():
-    g.db = get_db()
+
 # This method returns students 
 # list and by default method will be GET
 @app.route('/api/getuser', methods = ['POST'])
@@ -19,13 +14,17 @@ def getuser():
 def updateuser():
     return update_user()
 
+@app.route('/api/regularsearch', methods=['POST'])
+def regularsearch():
+    return regular_search()
+
 @app.route('/api/getrecommendation', methods=['POST'])
 def getrecommendations():
     return get_recommendations()
 
-@app.route('/api/getuservector', methods=['POST'])
-def getuservector():
-    return get_user_vector()
+@app.route('/api/personalizedsearch', methods=['POST'])
+def personalizedsearch():
+    return personalized_search()
 # This method executes after every API request.
 @app.after_request
 def after_request(response):
